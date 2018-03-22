cbfb545a872d4cf7048eadcbaf0a2e95cc47d4a8
Keller Jordan <kellerjordan1998@gmail.com>
2018-03-21 23:03:20 -0700

    implement exponentiated gradient optimizer

--- egpm.py
+++ egpm.py
@@ -1,20 +1,16 @@
+mplementation of EG plus/minus optimizer in PyTorch
+
+Author: Keller Jordan
+"""
+
 import torch
-from torch.optim.optimizer immport Optimizer, required
+from torch.optim.optimizer import Optimizer, required
 
-class SGD(Optimizer):
+class EGPM(Optimizer):
 
-    def __init__(self, params, lr=required, momentum=0, dampening=0,
-                 weight_decay=0, nesterov=False):
-        defaults = dict(lr=lr, momentum=momentum, dampening=dampening,
-                        weight_decay=weight_decay, nesterov=nesterov)
-        if nesterov and (momentum <= 0 or dampening != 0):
-            raise ValueError("Nesterov momentum requires a momentum and zero dampening")
-        super(SGD, self).__init__(params, defaults)
-
-    def __setstate__(self, state):
-        super(SGD, self).__setstate__(state)
-        for group in self.param_groups:
-            group.setdefault('nesterov', False)
+    def __init__(self, params, lr=required, u_scaling=100):        
+        defaults = dict(lr=lr, u_scaling=u_scaling)
+        super().__init__(params, defaults)
 
     def step(self, closure=None):
         loss = None
@@ -22,30 +18,34 @@ class SGD(Optimizer):
             loss = closure()
 
         for group in self.param_groups:
-            weight_decay = group['weight_decay']
-            momentum = group['momentum']
-            dampening = group['dampening']
-            nesterov = group['nesterov']
+            lr = group['lr']
+            u = group['u_scaling']
 
             for p in group['params']:
                 if p.grad is None:
                     continue
                 d_p = p.grad.data
-                if weight_decay != 0:
-                    d_p.add_(weight_decay, p.data)
-                if momentum != 0:
-                    param_state = self.state[p]
-                    if 'momentum_buffer' not in param_state:
-                        buf = param_state['momentum_buffer'] = torch.zeros_like(p.data)
-                        buf.mul_(momentum).add_(d_p)
-                    else:
-                        buf = param_state['momentum_buffer']
-                        buf.mul_(momentum).add_(1 - dampening, d_p)
-                    if nesterov:
-                        d_p = d_p.add(momentum, buf)
-                    else:
-                        d_p = buf
+                
+                param_state = self.state[p]
+                # initialize positive and negative weights
+                if 'egpos' not in param_state:
+                    param_state['egpos'] = (u + p.data.clone()) / 2
+                    param_state['egneg'] = (u - p.data.clone()) / 2
+                
+                egpos = param_state['egpos']
+                egneg = param_state['egneg']
+                
+                rpos = torch.exp(-lr * d_p)
+                rneg = torch.exp(lr * d_p)
+                updatepos = rpos * egpos
+                updateneg = rneg * egneg
+                
+                # constant to normalize sum of weights to 1
+                Z = torch.mean(updatepos + updateneg)
+                
+                egpos = param_state['egpos'] = u * updatepos / Z
+                egneg = param_state['egneg'] = u * updateneg / Z
 
-                p.data.add_(-group['lr'], d_p)
-
+                p.data = egpos - egneg
+    
         return loss
--- main.py
+++
@@ -1,3 +0,0 @@
-"""
-Run ResNet34 model on CIFAR10 using SGD and EG+-
-"""
